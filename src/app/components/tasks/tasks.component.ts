d7725bdd1b1d832e5062d569cf629ecdfb384571
soufiane000 <site.web97@gmail.com>
2021-06-09 00:33:36 +0100

    ....

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/app/models/task';
+import { TskService } from 'src/app/services/tsk.service';
 
 @Component({
   selector: 'app-tasks',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TasksComponent implements OnInit {
 
-  constructor() { }
+  tasks: Task[] = [];
+  constructor(private taskService: TskService) { }
 
   ngOnInit(): void {
+    this.getTasks();
+  }
+  getTasks(){
+    this.taskService.findAll()
+    .subscribe(tasks => this.tasks = tasks)
+  }
+  deleteTask(id){
+    this.taskService.delete(id)
+    .subscribe(() => {
+      this.tasks = this.tasks.filter(task => task.id != id)
+    })
   }
 
 }
--- src/app/services/tsk.service.ts
+++ src/app/services/tsk.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Task } from '../models/task';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +8,11 @@ import { Injectable } from '@angular/core';
 export class TskService {
 
   constructor(private http: HttpClient) { }
+  
+  findAll() {
+    return this.http.get<Task[]>("http://localhost:3000/tasks");
+  }
+  delete(id) {
+    return this.http.delete(`http://localhost:3000/tasks/${id}`)
+  }
 }
